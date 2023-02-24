fcc60e6bda547b7e3c21bd0bec94b4d07fec0d6e
San-Tri <1473024@senati.pe>
2023-02-24 10:37:47 -0500

    trabajando en python

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+# Desarrolle un algoritmo en la que muestre la suma del número actual y el siguiente (10):
+suma = 0
+x = 1
+for x in range(10):
+    while True:
+         
+        suma = suma + n
+        x += 1
+print("el algoritmo: ")
